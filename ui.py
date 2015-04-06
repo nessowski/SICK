7f308a490dee01689671b3ce04a767d353da3f17
Marnix Massar <weloxux@glowbug.nl>
2015-04-06 19:56:19 +0200

    Got basic GUI working.

---
+++ ui.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# SICK is an IRC client.
+# Please refer to README.md for more details.
+#
+# This file is released under the GNU General Public License v2.0,
+# of which you should have received a copy with your download.
+# If not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.txt
+#
+# Author: Marnix "Weloxux" Massar <weloxux@glowbug.nl>
+
+import os
+import sys
+import platform
+from sys import stdout as stdout
+
+sys.path.append("lib")
+from termcolor import *
+
+
+VERSION = "SICK alpha v0.0.1a"
+OS = platform.system() + " " + platform.release()
+
+
+HEIGHT, WIDTH = os.popen('stty size', 'r').read().split()
+
+def drawtopbar(width):
+
+	# First line:
+	stdout.write( colored("╔", "red", attrs=["dark"] ) )
+
+	for i in xrange(int(width) - 2):
+		mid = int(width) / 2 - 1
+		if i != mid:
+			stdout.write( colored("═", "red", attrs=["dark"] ) )
+		else:
+			stdout.write( colored("╦", "red", attrs=["dark"] ) )
+
+	stdout.write( colored("╗", "red", attrs=["dark"] ) )
+
+	# Second line:
+	stdout.write( colored("║", "red", attrs=["dark"] ) )
+	beginwriting = ( int(WIDTH) / 2 ) - ( VERSION.__len__() ) - 2
+
+	i = 0
+	
+	while i < int(width):
+		if i < beginwriting:
+			stdout.write(" ")
+		elif i == beginwriting:
+			stdout.write( colored( VERSION + " ║ " + OS, "red", attrs=["dark"] ) )
+			i += VERSION.__len__() + OS.__len__() + 4
+		elif i >= int(width):
+			pass #This can probably be done nicer
+		else:
+			stdout.write(" ")
+		i += 1
+	
+	stdout.write( colored("║", "red", attrs=["dark"] ) )
+
+	# Third line:
+        stdout.write( colored("╚", "red", attrs=["dark"] ) )
+
+        for i in xrange(int(width) - 2):
+                mid = int(width) / 2 - 1
+                if i != mid:
+                        stdout.write( colored("═", "red", attrs=["dark"] ) )
+                else:
+                        stdout.write( colored("╩", "red", attrs=["dark"] ) )
+
+        stdout.write( colored("╝", "red", attrs=["dark"] ) )
+
+def main():
+	while True:
+		pass
+
+
+if __name__ == "__main__":
+	drawtopbar(WIDTH)
+
+	i = 0
+	while i < int(HEIGHT) - 3:
+		stdout.write("\n")
+		i += 1
+
+	main()
+
+#EOF
